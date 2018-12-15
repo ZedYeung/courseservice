9c8dc90a64e9e5de86afa4c99c7b9a4bc36a34b9
Yue (Zed) Yang <shoominga@gmail.com>
2018-12-16 02:15:34 +0800

    update CoursesResource

--- courseservice/src/main/java/com/csye6225/fall2018/courseservice/resources/CoursesResource.java
+++ courseservice/src/main/java/com/csye6225/fall2018/courseservice/resources/CoursesResource.java
@@ -1,58 +1,50 @@
 package com.csye6225.fall2018.courseservice.resources;
 
-import java.util.List;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 import com.csye6225.fall2018.courseservice.datamodel.Course;
 import com.csye6225.fall2018.courseservice.service.CoursesService;
 
-@Path("courses")
-public class CoursesResource {
+@Path("course")
+public class CourseResource {
 
-	CoursesService courseService = new CoursesService();
-	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<Course> getAllCourses() {
-		return courseService.getAllCourses();
-	}
-	 
-	@GET
-	@Path("/{courseId}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Course getCourseById(@PathParam("courseId") long courseId) {
-		return courseService.getCourse(courseId);
-	}
-	
-	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Course addCourse(Course course) {
-		return courseService.addCourse(course);
-	}
-	
-	@PUT
-	@Path("/{courseId}")
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Course updateProfessor(@PathParam("courseId") long courseId, Course newCourse) {
-		return courseService.updateCourse(courseId, newCourse);
-	}
-	
-	@DELETE
-	@Path("/{courseId}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Course deleteCourse(@PathParam("courseId") long courseId) {
-		return courseService.deleteCourse(courseId);
-	}
- }
+    CourseService courseService = new CourseService();
+
+		@POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Course add(Course course) {
+        return courseService.add(course);
+    }
+
+    @DELETE
+    @Path("/{courseId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Course delete(@PathParam("courseId") String courseId) {
+        return courseService.delete(courseId);
+    }
+
+    @PUT
+    @Path("/{courseId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Course update(@PathParam("courseId") String courseId, Course course) {
+        return courseService.update(course);
+    }
+
+		@GET
+    @Path("/{courseId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Course get(@PathParam("courseId") String courseId) {
+        return courseService.get(courseId);
+    }
+
+		@GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Course> getAll() {
+        return courseService.getAll();
+    }
+}
+
