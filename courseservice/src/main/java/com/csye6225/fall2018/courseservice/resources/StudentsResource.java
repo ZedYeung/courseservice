53aeabc9c3b0cfb95f18472b59624cea283b649f
Yue (Zed) Yang <shoominga@gmail.com>
2018-12-16 02:15:49 +0800

    update StudentsResource

--- courseservice/src/main/java/com/csye6225/fall2018/courseservice/resources/StudentsResource.java
+++ courseservice/src/main/java/com/csye6225/fall2018/courseservice/resources/StudentsResource.java
@@ -1,58 +1,50 @@
 package com.csye6225.fall2018.courseservice.resources;
 
-import java.util.List;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 import com.csye6225.fall2018.courseservice.datamodel.Student;
 import com.csye6225.fall2018.courseservice.service.StudentsService;
 
-@Path("students")
-public class StudentsResource {
+@Path("student")
+public class StudentResource {
 
-	StudentsService studentService = new StudentsService();
-	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<Student> getAllStudents() {
-		return studentService.getAllStudents();
-	}
-	 
-	@GET
-	@Path("/{studentId}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Student getStudentById(@PathParam("studentId") long studentId) {
-		return studentService.getStudent(studentId);
-	}
-	
-	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Student addStudent(Student student) {
-		return studentService.addStudent(student);
-	}
-	
-	@PUT
-	@Path("/{studentId}")
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Student updateProfessor(@PathParam("studentId") long studentId, Student newStudent) {
-		return studentService.updateStudent(studentId, newStudent);
-	}
-	
-	@DELETE
-	@Path("/{studentId}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Student deleteStudent(@PathParam("studentId") long studentId) {
-		return studentService.deleteStudent(studentId);
-	}
- }
+    StudentService studentService = new StudentService();
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Student add(Student student) {
+        return studentService.add(student);
+    }
+
+		@DELETE
+    @Path("/{studentId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Student delete(@PathParam("studentId") String studentId) {
+        return studentService.delete(studentId);
+    }
+
+    @PUT
+    @Path("/{studentId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Student update(@PathParam("studentId") String studentId, Student student) {
+        return studentService.update(student);
+    }
+
+		@GET
+    @Path("/{studentId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Student get(@PathParam("studentId") String studentId) {
+        return studentService.get(studentId);
+    }
+
+		@GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Student> getAll() {
+        return studentService.getAll();
+    }
+}
+
