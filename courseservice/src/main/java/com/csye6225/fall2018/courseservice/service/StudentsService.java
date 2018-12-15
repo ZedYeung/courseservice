1f16b0fcc2db3766285d1069478564b620005ddb
Yue (Zed) Yang <shoominga@gmail.com>
2018-12-15 15:09:50 +0800

    update StudentsService

--- courseservice/src/main/java/com/csye6225/fall2018/courseservice/service/StudentsService.java
+++ courseservice/src/main/java/com/csye6225/fall2018/courseservice/service/StudentsService.java
@@ -1,49 +1,59 @@
 package com.csye6225.fall2018.courseservice.service;
-
-import com.csye6225.fall2018.courseservice.datamodel.InMemoryDatabase;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDeleteExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.csye6225.fall2018.courseservice.datamodel.DynamoDBConnector;
 import com.csye6225.fall2018.courseservice.datamodel.Student;
-import java.util.*;
 
-public class StudentsService {
-	static HashMap<Long, Student> studentsMap = InMemoryDatabase.getStudentDB();
-	
-	// Getting a list of all students
-	// GET "..webapi/students"
-	public List<Student> getAllStudents() {	
-		List<Student> list = new ArrayList<>();
-		for (Student student : studentsMap.values()) {
-			list.add(student);
-		}
-		return list ;
-	}
-	
-	//GET, get a student by its id
-	public Student getStudent(long studentId) {
-		return studentsMap.get(studentId);
-	}
+import java.util.HashMap;
+import java.util.List;
 
-	//POST, add a new student
-	public Student addStudent(Student student) {
-		long nextAvailableId = studentsMap.size() + 1;
-		student.setStudentId(nextAvailableId);
-		studentsMap.put(nextAvailableId, student);
-		return student;
-	}
-	
-	//PUT, update information of a student
-	public Student updateStudent(long studentId, Student student) {	
-//		Student oldStudent = studentsMap.get(studentId);
-//		studentId = oldStudent.getStudentId();
-//		student.setStudentId(studentId);
-		studentsMap.put(studentId, student);
-		return student;
-	}
-	
-	//DELETE, delete a student
-	public Student deleteStudent(long studentId) {
-		Student deletedStudentInfo = studentsMap.get(studentId);
-		studentsMap.remove(studentId);
-		return deletedStudentInfo;
-	}
+public class StudentService {
 
+    static DynamoDBConnector dynamoDb;
+    DynamoDBMapper mapper;
+
+    public StudentService() {
+        dynamoDb = new DynamoDBConnector();
+        dynamoDb.init();
+        mapper = new DynamoDBMapper(dynamoDb.getClient());
+    }
+
+		public Student add(Student student) {
+        mapper.save(student);
+        return student;
+    }
+
+		public Student delete(String studentId) {
+        Student student = get(studentId);
+        mapper.delete(student, new DynamoDBDeleteExpression());
+        return student;
+    }
+
+    public Student update(Student student) {
+        delete(student.getStudentId());
+        mapper.save(student);
+        return student;
+    }
+
+    public Student get(String studentId) {
+        HashMap<String, AttributeValue> eav = new HashMap<>();
+        eav.put(":v1",  new AttributeValue().withS(studentId));
+
+        DynamoDBQueryExpression<Student> queryExpression = new DynamoDBQueryExpression<Student>()
+                .withIndexName("studentId-index")
+                .withKeyConditionExpression("studentId = :v1")
+                .withConsistentRead(false)
+                .withExpressionAttributeValues(eav);
+
+        List<Student> list =  mapper.query(Student.class, queryExpression);
+        if (list.size() == 0) return null;
+        return list.get(0);
+    }
+
+    public List<Student> getAll(){
+        return mapper.scan(Student.class, new DynamoDBScanExpression());
+    }
 }
