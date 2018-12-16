2dadf901e2ac5dfd7e648f9b5ed67803b6a1dfb8
Yue (Zed) Yang <shoominga@gmail.com>
2018-12-16 12:21:27 +0800

    fix: use iam to access aws resource rather than credential file

--- courseservice/src/main/java/com/csye6225/fall2018/courseservice/datamodel/DynamoDBConnector.java
+++ courseservice/src/main/java/com/csye6225/fall2018/courseservice/datamodel/DynamoDBConnector.java
@@ -1,6 +1,6 @@
 package com.csye6225.fall2018.courseservice.datamodel;
 
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 
@@ -10,15 +10,23 @@ public class DynamoDBConnector {
 
 	 public static void init() {
 		if (dynamoDb == null) {
-		ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
-		credentialsProvider.getCredentials();
+			// ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
+			//
+			// credentialsProvider.getCredentials();
+			//
+			// dynamoDb = AmazonDynamoDBClientBuilder
+			// 			.standard()
+			// 			.withCredentials(credentialsProvider)
+			// 			.withRegion("us-east-1")
+			// 			.build();
 
-		dynamoDb = AmazonDynamoDBClientBuilder
+			dynamoDb = AmazonDynamoDBClientBuilder
 					.standard()
-					.withCredentials(credentialsProvider)
+	        .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
 					.withRegion("us-east-1")
 					.build();
-		System.out.println("I created the client");
+
+			System.out.println("I created the client");
 		}
 	}
 
